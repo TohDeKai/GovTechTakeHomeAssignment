a5ee7b5b0079c5e9ba62315700c195077cb96b1f
TohDeKai <tohdekai@outlook.com>
2022-02-15 14:04:44 +0800

    naming routes

--- main.py
+++ main.py
@@ -14,65 +14,70 @@ JWT_SECRET = "Hello world"
 
 class User(Model):
     id = fields.IntField(pk=True)
-    username = fields.CharField(50, unique = True)
+    username = fields.CharField(50, unique=True)
     password_hash = fields.CharField(128)
 
     def verify_password(self, password):
         return bcrypt.verify(password, self.password_hash)
-    
-User_Pydantic = pydantic_model_creator(User, name = "User")
-UserIn_Pydantic = pydantic_model_creator(User, name = "UserIn", exclude_readonly = True)
 
-ouath2_scheme = OAuth2PasswordBearer(tokenUrl = "token")
+User_Pydantic = pydantic_model_creator(User, name='User')
+UserIn_Pydantic = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='user/login')
 
 async def authenticate_user(username: str, password: str):
     user = await User.get(username=username)
     if not user:
-        return False
+        return False 
     if not user.verify_password(password):
         return False
-    return user
+    return user 
+
+@app.post('/user/register', response_model=User_Pydantic)
+async def create_user(user: UserIn_Pydantic):
+    user_obj = User(username=user.username, password_hash=bcrypt.hash(user.password_hash))
+    await user_obj.save()
+    return await User_Pydantic.from_tortoise_orm(user_obj)  
 
 # Endpoint to be called to generate token
 # Token will represent who is logged in
-@app.post("/token")
+@app.post('/user/login')
 async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = await authenticate_user(form_data.username, form_data.password)
 
     if not user:
-        return {"error" : "invalid credentials"}
-    
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
+
     user_obj = await User_Pydantic.from_tortoise_orm(user)
 
     token = jwt.encode(user_obj.dict(), JWT_SECRET)
 
-    return {"access_token" : token, "token_type" : "bearer"}
+    return {'access_token' : token, 'token_type' : 'bearer'}
 
-async def get_user_current(token: str = Depends(ouath2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms = ["HS256"]) #The default algorithm
-        user = await User.get(id = payload.get("id"))
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256']) #The default algorithm
+        user = await User.get(id=payload.get('id'))
     except:
         raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED, 
-            detail = "Invalid username or password. Please try again.")
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
 
+    return await User_Pydantic.from_tortoise_orm(user)
 
-@app.post("/users", response_model= User_Pydantic)
-async def create_user(user: UserIn_Pydantic):
-    user_obj = User(username = user.username, password_hash = bcrypt.hash(user.password_hash))
-    await user_obj.save()
-    return await User_Pydantic.from_tortoise_orm(user_obj)
-
-@app.get("/users/me", response_model= User_Pydantic)
-async def get_user(user: User_Pydantic = Depends(get_user_current)):
-    return user
+@app.get('/user/profile', response_model=User_Pydantic)
+async def get_user(user: User_Pydantic = Depends(get_current_user)):
+    return user  
 
 register_tortoise(
-    app,
-    db_url = "sqlite://db.sqlite3",
-    modules = {"models" : ["main"]},
-    generate_schemas = True,
-    add_exception_handlers = True
+    app, 
+    db_url='sqlite://db.sqlite3',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
 )
 
